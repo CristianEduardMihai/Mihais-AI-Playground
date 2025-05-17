30f4705c45fbc26edcde66999cc7ba8e2776a60b
CristianEduardMihai <cristianeduardmihai@gmail.com>
2025-05-17 17:50:13 +0300

    Fixed static icon router I'm sure there's a better way to do this

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.staticfiles import StaticFiles
 from reactpy.backend.fastapi import configure, Options
 
@@ -30,6 +30,36 @@ configure(
 # for future
 # configure(app, CalorieTracker, Options(url_prefix="/health/calorie_tracker"))
 # configure(app, RoastBattle,  Options(url_prefix="/health/fun/roast-battle"))
+
+
+# Apple icons and favicons at root for iOS/browser compatibility
+@app.get("/favicon.ico")
+def favicon():
+    return FileResponse("static/favicon.ico")
+
+@app.get("/apple-touch-icon.png")
+def apple_icon():
+    return FileResponse("static/favicon_io/apple-touch-icon.png")
+
+@app.get("/android-chrome-192x192.png")
+def android_192():
+    return FileResponse("static/favicon_io/android-chrome-192x192.png")
+
+@app.get("/android-chrome-512x512.png")
+def android_512():
+    return FileResponse("static/favicon_io/android-chrome-512x512.png")
+
+@app.get("/favicon-16x16.png")
+def favicon_16():
+    return FileResponse("static/favicon_io/favicon-16x16.png")
+
+@app.get("/favicon-32x32.png")
+def favicon_32():
+    return FileResponse("static/favicon_io/favicon-32x32.png")
+
+@app.get("/site.webmanifest")
+def site_manifest():
+    return FileResponse("static/favicon_io/site.webmanifest")
 
 if __name__ == "__main__":
     import uvicorn
